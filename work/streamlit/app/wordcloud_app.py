e6cec6f8c5db264a39c4008cc5ce88c3be4c9c1a
Roland Bauer <roland6bauer@gmail.com>
2023-01-08 11:46:41 +0100

    added wordcloud_app.py

---
+++ work/streamlit/app/wordcloud_app.py
@@ -0,0 +1,172 @@
+import streamlit as st
+# from streamlit_autorefresh import st_autorefresh
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from Kafka_Helpers import Consumer, Producer
+import time
+import ast
+import json
+import threading
+import pandas as pd
+
+header = st.empty()
+subheader = st.empty()
+cloud = st.empty()
+list_count = st.empty()
+loading = False
+movie_id = 0
+movie_title = ""
+timeout_occurred = False
+
+def prepare_data(data, default=(None, None)):
+    global movie_id, movie_title
+    if data is not None:
+        movie_id = data['movie_id']
+        movie_title = data['title']
+        l = ast.literal_eval(str(data['reviews']))
+        temp = [item for sublist in l for item in sublist]
+        if len(movie_title) != 0 and len(temp) == 0:
+            return default
+        word_list = ", ".join(temp)
+        counted_words = data['counted_words']
+        return word_list, counted_words
+    else:
+        return default
+
+def write_movie_to_mongodb(value):
+    o = json.loads(value)
+    o['movie_id'] = str(o['movie_id']).replace("'", '').replace('[', '').replace(']', '')
+    o['title'] = str(o['title']).replace("'", '').replace('[', '').replace(']', '')
+    o['reviews'] = ast.literal_eval(str(o['reviews']))
+    
+    if not check_if_movie_exists_in_mongodb(movie_id):
+        from pymongo import MongoClient
+        client = MongoClient('localhost', 27017,
+                        username='root',
+                        password='rootpassword')
+        db = client['movies']
+        collection = db['reviews']
+        # text = text.replace("'", '"')
+        collection.insert_one(o)
+
+def check_if_movie_exists_in_mongodb(movie_id):
+    try:
+        from pymongo import MongoClient
+        client = MongoClient('localhost', 27017,
+                     username='root',
+                     password='rootpassword')
+        db = client['movies']
+        collection = db['reviews']
+        res = collection.find_one({'movie_id': str(movie_id)})
+        if res is not None:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+def load_movie_from_mongodb_if_exists(movie_id, default=(None, None)):
+    from pymongo import MongoClient
+    client = MongoClient('localhost', 27017,
+                    username='root',
+                    password='rootpassword')
+    db = client['movies']
+    collection = db['reviews']
+    res = collection.find_one({'movie_id': movie_id})
+    if res is not None:
+        return prepare_data(res, default)
+    else:
+        return default
+
+def handle_message(key, value):
+    global loading
+    loading = False
+    write_movie_to_mongodb(value)
+    time.sleep(0.5)
+    loading = False
+
+def get_wordcloud(text, counted_words):
+    # text, counted_words = load_text_from_file()
+    if text is not None:
+        wordcloud = WordCloud(max_words=20).generate(text)
+
+        fig, ax = plt.subplots()
+        ax.axis('off')
+        ax.imshow(wordcloud, interpolation='bilinear')
+        header.title(movie_title)
+        subheader.subheader("Wordcloud for movie id: " + str(movie_id))
+        cloud.pyplot(fig)
+        list_count.subheader("Top 30 most frequent words:")
+        # sort counted_words by value
+        df = pd.DataFrame(counted_words, columns=['word', 'count'])
+        df = df.sort_values(by=['count'], ascending=False)
+        list_count.table(df.head(30))
+    else:
+        st.info(f"No reviews found for {movie_title} with id {movie_id}")
+
+def check_timeout(timeout, handler):
+    time.sleep(timeout)
+    handler(True)
+
+def set_timeout(new_state):
+    global timeout_occurred
+    timeout_occurred = new_state
+
+def prepare_sidebar():
+    st.sidebar.title("Movie Wordcloud Generator")
+    input_no = st.sidebar.text_input("Enter any movie-id :point_down:")
+    button_state = st.sidebar.button("Show me the wordcloud!")
+    st.sidebar.write("(33, 77, 76600)")
+    st.sidebar.markdown("---")
+    button_most_pop = st.sidebar.button("Show most popular movie")
+    st.sidebar.markdown("---")
+    button_most_blabla = st.sidebar.button("Show most blabla movie")
+    st.sidebar.markdown("---")
+    button_clear_db = st.sidebar.button("Clear Cache-MongoDB")
+    return input_no, button_state, button_clear_db, button_most_pop, button_most_blabla
+
+Consumer(server='localhost', port=29092, topic_name='adjectives_counted', handler=handle_message)
+my_producer = Producer(server='localhost', port=29092)
+
+input_no, button_state, button_clear_db, button_most_pop, button_most_blabla = prepare_sidebar()
+
+if button_state or input_no:
+    # check if input_no is a number
+    if input_no.isnumeric():
+        loading = True
+        text, counted_words = (None, None)
+
+        if check_if_movie_exists_in_mongodb(input_no) == False:
+            with st.spinner('Data not in Cache-MongoDB, loading from API :wink:...'):
+                key_bytes = bytes('data', encoding='utf-8')
+                my_producer.send(topic_name='new_movie_id', key=key_bytes, value=input_no)
+                x = threading.Thread(target=check_timeout, args=(10, set_timeout))
+                x.start()
+                while loading and not timeout_occurred:
+                    time.sleep(0.1)
+
+        if timeout_occurred:
+            st.error("Timeout occurred. Please try again later.")
+        else:
+            time.sleep(1)
+            text, counted_words = load_movie_from_mongodb_if_exists(input_no)
+            if text is None:
+                st.error("No reviews found for movie-id: " + input_no)
+            else:
+                get_wordcloud(text, counted_words)
+    else:
+        st.sidebar.error("Please enter a movie-id :point_up:")
+
+if button_most_pop:
+    st.sidebar.warning("Not implemented yet :sweat_smile:")
+
+if button_most_blabla:
+    st.sidebar.warning("Not implemented yet :sweat_smile:")
+
+if button_clear_db:
+    from pymongo import MongoClient
+    client = MongoClient('localhost', 27017, username='root', password='rootpassword')
+    db = client['movies']
+    collection = db['reviews']
+    collection.delete_many({})
+    st.sidebar.success("Cache cleared! :sunglasses:")
